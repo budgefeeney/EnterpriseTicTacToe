e651799a4f4ffa5050989f6061d838a134a98343
Bryan Feeney <bryan.feeney@gmail.com>
2015-05-30 23:09:00 +0100

    Got Main program loop working
    Including quit and error handling

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import           Control.Monad (forM_)
 import           TicTacToe
+import           FSharpisms ((<|))
 
 displayBoard :: GameState -> IO ()
 displayBoard = print
@@ -9,32 +10,57 @@ displayBoard = print
 numberedMoves :: [a] -> [(Int, a)]
 numberedMoves = zip [0..]
 
-printAsNumberedLines :: (Show a) => [a] ->  IO ()
+printAsNumberedLines :: (Show a) => [a] -> IO ()
 printAsNumberedLines ls =
     forM_ (numberedMoves ls) $ \(n,l) ->
         putStrLn <| show n ++ " - " ++ show l
 
-processMove :: (GameState, MoveResult) -> IO (GameState, MoveResult)
-processMove (game, m@(GameWon p)) = error "Game is already won, shouldn't be asking for moves"
-processMove (game, m@GameTied)    = error "Game is tied, shouldn't be asking for moves"
+processMove :: (GameState, MoveResult) -> IO (MoveResult)
+processMove (game, m@(GameWon p)) = return m
+processMove (game, m@GameTied)    = return m
 processMove (game, m@(PlayerXsTurn (ValidXMoves xms))) =
-    do
-        displayBoard game
+  do
+      displayBoard game
 
-        putStrLn "\nPlayer Xs turn"
-        printAsNumberedLines xms
-        putStrLn "Q - Quit"
+      putStrLn "\nPlayer Os turn"
+      printAsNumberedLines oms
+      putStrLn "Q - Quit"
 
-        cmd <- getLine
-        let intCmd = read cmd :: Int
+      cmd <- getLine
+      case cmd of
+        "Q" -> return GameQuit
+        _   -> case (reads cmd) :: [(Int, String)] of
+                  [(intCmd, "")] -> processMove <| playerOMove game (oms !! intCmd)
+                  _              -> do
+                                      putStrLn <| "Invalid input '" ++ cmd ++ "', try again"
+                                      processMove (game, m)
+processMove (game, m@(PlayerOsTurn (ValidOMoves oms))) =
+  do
+      displayBoard game
 
-        putStrLn $ "You chose " ++ show cmd
-        return (game, m)
+      putStrLn "\nPlayer Os turn"
+      printAsNumberedLines oms
+      putStrLn "Q - Quit"
 
+      cmd <- getLine
+      case cmd of
+        "Q" -> return GameQuit
+        _   -> case (reads cmd) :: [(Int, String)] of
+                  [(intCmd, "")] -> processMove <| playerOMove game (oms !! intCmd)
+                  _              -> do
+                                      putStrLn <| "Invalid input '" ++ cmd ++ "', try again"
+                                      processMove (game, m)
 processMove _ = error "Unsupported"
 
+
 main::IO()
-main = do
-   let (state, move) = newGame
-   (s', m') <- processMove (state, move)
-   return ()
+main =
+  let printResult (GameWon p) = putStrLn <| "Game has been won by player " ++ show p
+      printResult (GameTied)  = putStrLn <| "Game has been tied"
+      printResult (GameQuit)  = putStrLn <| "Game has been quit by user "
+      printResult _           = putStrLn <| "Game Ended Weirdly"
+  in
+  do
+    let (state, move) = newGame
+    m <- processMove (state, move)
+    printResult m
